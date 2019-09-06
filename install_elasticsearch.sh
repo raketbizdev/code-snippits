1fd4dd8c1e1b7196d879311ef072d3fcfa044089
Ruel Nopal <radon1284@yahoo.com>
2019-09-07 04:44:07 +0800

    Create install_elasticsearch.sh

---
+++ install_elasticsearch.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Author name: Ruel Nopal
+# Company: Ruel Nopal - IT Consultant.
+# url: www.ruelnopal.com
+# Installing Elasticsearch to aws instance.
+
+# change version before running.
+
+java -version
+
+echo $JAVA_HOME
+# if not available add java home to .bash_profile
+
+export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+
+export PATH=$PATH:/usr/lib/jvm/java-1.11.0-openjdk-amd64
+
+sudo apt-get install apt-transport-https
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo add-apt-repository "deb https://artifacts.elastic.co/packages/7.x/apt stable main"
+
+sudo apt-get update
+sudo apt-get install elasticsearch
+
+sudo nano /etc/elasticsearch/elasticsearch.yml
+
+ network.host: 0.0.0.0
+ cluster.name: myCluster1
+ node.name: "myNode1"
+ 
+ sudo /bin/systemctl enable elasticsearch.service
