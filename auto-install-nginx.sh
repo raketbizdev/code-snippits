edec442906c39f076e48310a62399d97cbb254ae
Ruel Nopal <radon1284@yahoo.com>
2019-12-27 19:28:32 +0800

    Create auto-install-nginx.sh

---
+++ auto-install-nginx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author name: Ruel Nopal
+# Company: Ruel Nopal - IT Consultant.
+# url: www.ruelnopal.com
+# Installing Apache in AWS inctance.
+
+echo "Make sure you are a sudo user"
+cd /
+sudo apt update
+sudo apt install apache2
+sudo ufw app list
+echo "Check if theres no error"
+sudo systemctl status apache2
+echo "Visit IP address below to check if works."
+curl http://checkip.amazonaws.com
+echo "Deleting Shell files."
+sudo rm -rf auto-install-apache2-aws.sh
