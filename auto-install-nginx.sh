2541642de767efdad7e48e6a06986e717a130adf
Ruel Nopal <radon1284@yahoo.com>
2019-12-27 19:31:30 +0800

    Update auto-install-nginx.sh

--- auto-install-nginx.sh
+++ auto-install-nginx.sh
@@ -2,16 +2,18 @@
 # Author name: Ruel Nopal
 # Company: Ruel Nopal - IT Consultant.
 # url: www.ruelnopal.com
-# Installing Apache in AWS inctance.
+# Installing NginX.
 
 echo "Make sure you are a sudo user"
 cd /
 sudo apt update
-sudo apt install apache2
+sudo apt install nginx
 sudo ufw app list
 echo "Check if theres no error"
-sudo systemctl status apache2
+sudo ufw allow 'Nginx HTTP'
+sudo ufw status
+systemctl status nginx
 echo "Visit IP address below to check if works."
 curl http://checkip.amazonaws.com
 echo "Deleting Shell files."
-sudo rm -rf auto-install-apache2-aws.sh
+sudo rm -rf auto-install-nginx.sh
