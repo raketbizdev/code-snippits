cef5389d37ec3620fc8fbd08334bd0b2f39274b8
Ruel Nopal <radon1284@yahoo.com>
2020-06-22 20:35:07 +0800

    mod fix

--- create-apache2-subdomain.sh
+++ create-apache2-subdomain.sh
@@ -15,10 +15,14 @@ echo 'change user'
 sudo chown $USER:$USER /var/www/html/${subdomain}
 sudo mkdir /var/www/html/${subdomain}/public
 sudo touch /var/www/html/${subdomain}/public/index.html
-echo '<h1>${subdomain} Test working</h1>' > /var/www/html/${subdomain}/public/index.html
+
+sudo cat >>  /var/www/html/${subdomain}/public/index.html <<EOL
+        <h1>${subdomain} Test working</h1>
+EOL
 echo 'create a virtualhost'
 sudo touch /etc/apache2/sites-available/${subdomain}.conf
-cat >> /etc/apache2/sites-available/${subdomain}.conf <<EOL
+
+sudo cat >> /etc/apache2/sites-available/${subdomain}.conf <<EOL
 <VirtualHost *:80>
 
         ServerAdmin webmaster@localhost
@@ -43,6 +47,6 @@ echo 'end of the commanline'
 echo 'deleting shell script'
 sudo cat /etc/apache2/sites-available/${subdomain}.conf
 sudo service apache2 restart
-a2ensite /etc/apache2/sites-available/${subdomain}.conf
+sudo a2ensite /etc/apache2/sites-available/${subdomain}.conf
 sudo rm create-apache2-subdomain.sh
 
