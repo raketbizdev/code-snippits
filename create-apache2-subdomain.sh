16d267a15f38cbae7ff05a6d14e694ccf1547f9e
Ruel Nopal <radon1284@yahoo.com>
2020-06-22 20:58:47 +0800

    mod fix

--- create-apache2-subdomain.sh
+++ create-apache2-subdomain.sh
@@ -15,14 +15,16 @@ echo 'change user'
 sudo chown $USER:$USER /var/www/html/${subdomain}
 sudo mkdir /var/www/html/${subdomain}/public
 sudo touch /var/www/html/${subdomain}/public/index.html
+sudo chown $USER:$USER /var/www/html/${subdomain}/public/index.html
 
 sudo cat >>  /var/www/html/${subdomain}/public/index.html <<EOL
         <h1>${subdomain} Test working</h1>
 EOL
 echo 'create a virtualhost'
-sudo touch /etc/apache2/sites-available/${subdomain}.conf
+sudo touch /var/www/html/${subdomain}/${subdomain}.conf
+sudo chown $USER:$USER /var/www/html/${subdomain}/${subdomain}.conf
 
-sudo cat >> /etc/apache2/sites-available/${subdomain}.conf <<EOL
+sudo cat >> /var/www/html/${subdomain}/${subdomain}.conf <<EOL
 <VirtualHost *:80>
 
         ServerAdmin webmaster@localhost
@@ -43,10 +45,12 @@ sudo cat >> /etc/apache2/sites-available/${subdomain}.conf <<EOL
 
 </VirtualHost>
 EOL
+sudo ln -nfs "/var/www/html/${subdomain}/${subdomain}.conf" "/etc/apache2/sites-enabled/${subdomain}.conf"
+sudo cat /var/www/html/${subdomain}/${subdomain}.conf
 echo 'end of the commanline'
 echo 'deleting shell script'
-sudo cat /etc/apache2/sites-available/${subdomain}.conf
+
 sudo service apache2 restart
-sudo a2ensite /etc/apache2/sites-available/${subdomain}.conf
+sudo a2ensite /var/www/html/${subdomain}/${subdomain}.conf
 sudo rm create-apache2-subdomain.sh
 
