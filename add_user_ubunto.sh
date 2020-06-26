c4a3d16191047c159a4ad9f6e6ff5f2bc07919b5
Ruel Nopal <radon1284@yahoo.com>
2020-06-26 11:45:05 +0800

    mod fix

--- add_user_ubunto.sh
+++ add_user_ubunto.sh
@@ -47,6 +47,6 @@ echo 'deleting add_user_ubunto.sh'
 echo 'if you like this script dont forget to spread the word and give a beer to this man.'
 echo 'my paypal https://paypal.me/freelancerdad'
 echo 'run this in your local machine'
-ip=curl http://checkip.amazonaws.com
-echo 'ssh ${username}@${ip}
+IP="curl http://checkip.amazonaws.com"
+echo 'ssh ${username}@${IP}
 sudo rm add_user_ubunto.sh
