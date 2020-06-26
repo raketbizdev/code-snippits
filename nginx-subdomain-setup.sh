cde07236a0f431b0775deb472d3d5fb72bba8683
Ruel Nopal <radon1284@yahoo.com>
2020-06-26 11:53:21 +0800

    initial

---
+++ nginx-subdomain-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author name: Ruel Nopal
+# Company: Ruel Nopal - IT Consultant.
+# url: www.ruelnopal.com
+# run the command below
+# wget https://raw.githubusercontent.com/raketbizdev/code-snippits/master/nginx-subdomain-setup.sh; sudo chmod 755 nginx-subdomain-setup.sh; ./nginx-subdomain-setup.sh;
