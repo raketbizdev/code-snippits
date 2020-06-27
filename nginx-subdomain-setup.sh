03c5fd86198df5615b357c637cbc673c792e995b
Ruel Nopal <radon1284@yahoo.com>
2020-06-27 19:01:28 +0800

    Update nginx-subdomain-setup.sh

--- nginx-subdomain-setup.sh
+++ nginx-subdomain-setup.sh
@@ -30,22 +30,22 @@ EOL
 echo -e '\e[33creating a virtualhost'
 sudo touch /var/www/html/${subdomain}/${subdomain}.conf
 sudo chown $USER:$USER /var/www/html/${subdomain}/${subdomain}.conf
-IP="curl http://checkip.amazonaws.com"
-GETIP="echo ${IP}"
-if which certbot >/dev/null; then
-    echo exists
-    echo -e '\e[33Cerbot already been install proceed to install cerbot'
-    sudo certbot certonly --nginx -d ${subdomain}
-else
-    echo does not exist
-    echo -e '\e[33Installing Cerbot letsencrypt'
-    sudo apt-get update
-    sudo apt-get install software-properties-common
-    sudo add-apt-repository universe
-    sudo apt-get update
-    sudo apt-get install certbot python3-certbot-nginx
-    sudo certbot certonly --nginx -d ${subdomain}
-fi
+# IP="curl http://checkip.amazonaws.com"
+# GETIP="echo ${IP}"
+# if which certbot >/dev/null; then
+#     echo exists
+#     echo -e '\e[33Cerbot already been install proceed to install cerbot'
+#     sudo certbot certonly --nginx -d ${subdomain}
+# else
+#     echo does not exist
+#     echo -e '\e[33Installing Cerbot letsencrypt'
+#     sudo apt-get update
+#     sudo apt-get install software-properties-common
+#     sudo add-apt-repository universe
+#     sudo apt-get update
+#     sudo apt-get install certbot python3-certbot-nginx
+#     sudo certbot certonly --nginx -d ${subdomain}
+# fi
 
 sudo cat >> /var/www/html/${subdomain}/${subdomain}.conf <<EOL
 server {
@@ -62,6 +62,7 @@ server {
 EOL
 sudo ln -nfs "/var/www/html/${subdomain}/${subdomain}.conf" "/etc/nginx/sites-enabled/${subdomain}.conf"
 sudo cat /var/www/html/${subdomain}/${subdomain}.conf
+sudo sudo nginx -t
 sudo systemctl restart nginx
 echo -e '\e[33end of the commanline'
 echo -e '\e[33deleting shell script'
@@ -70,4 +71,5 @@ echo -e '\e[33deleting nginx-subdomain-setup.sh'
 echo -e '\e[33if you like this script dont forget to spread the word and give a beer to this man.'
 echo -e '\e[33my paypal \e[5https://paypal.me/freelancerdad'
 echo -e '\e[33Enjoy happy coding'
+curl http://checkip.amazonaws.com
 sudo rm nginx-subdomain-setup.sh
