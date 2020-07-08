74a8ea9c374c2fb338d522790a8096f3cc049e40
Ruel Nopal <radon1284@yahoo.com>
2020-07-08 22:27:10 +0800

    create apache vhost

---
+++ oneclick-apache-vhost-create.sh
@@ -0,0 +1,35 @@
+  
+#!/bin/bash
+# Author name: Ruel Nopal
+# Company: Ruel Nopal - IT Consultant.
+# url: www.ruelnopal.com
+# run the command below
+# wget https://raw.githubusercontent.com/raketbizdev/code-snippits/master/oneclick-apache-vhost-create.sh; sudo chmod 755 oneclick-apache-vhost-create.sh; ./oneclick-apache-vhost-create.sh;
+
+echo 'create a subdomain folder'
+echo 'enter subdomain name:'
+read subdomain
+sudo touch ${subdomain}.conf
+sudo chown $USER:$USER ${subdomain}.conf
+dir_root=pwd
+sudo cat >> ${subdomain}.conf <<EOL
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        DocumentRoot $dir_root
+        ServerName ${subdomain}
+      
+        DirectoryIndex index.html index.cgi index.php
+        <Directory /var/www/html/${subdomain}/public/>
+                Options Indexes FollowSymLinks
+                AllowOverride All
+                Require all granted
+        </Directory>
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOL
+sudo ln -nfs "$dir_root/${subdomain}.conf" "/etc/apache2/sites-enabled/${subdomain}.conf"
+sudo systemctl restart apache2
+echo 'end of the commanline'
+echo 'deleting shell script'
+sudo rm create-apache2-subdomain.sh
