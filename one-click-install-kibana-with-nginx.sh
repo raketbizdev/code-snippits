af14e037e5907e3bf1aa9efcccbee251ba3df22e
Ruel Nopal <radon1284@yahoo.com>
2020-07-23 22:31:13 +0800

    Update one-click-install-kibana-with-nginx.sh

--- one-click-install-kibana-with-nginx.sh
+++ one-click-install-kibana-with-nginx.sh
@@ -48,10 +48,10 @@ server {
         location / {
             proxy_pass http://localhost:5601;
             proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Upgrade \$http_upgrade;
             proxy_set_header Connection 'upgrade';
-            proxy_set_header Host $host;
-            proxy_cache_bypass $http_upgrade;        
+            proxy_set_header Host \$host;
+            proxy_cache_bypass \$http_upgrade;        
         }
     }
 EOL
