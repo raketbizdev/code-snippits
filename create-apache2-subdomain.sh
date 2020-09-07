2993eb9ba469bdaccadd993bfa343ce964475675
Ruel Nopal <radon1284@yahoo.com>
2020-09-07 15:14:17 +0800

    Update create-apache2-subdomain.sh

--- create-apache2-subdomain.sh
+++ create-apache2-subdomain.sh
@@ -18,7 +18,8 @@ sudo chown $USER:$USER ${subdomain}
 sudo mkdir ${subdomain}/public
 sudo touch ${subdomain}/public/index.html
 sudo chown $USER:$USER ${subdomain}/public/index.html
-
+echo 'creating ssl from cerbot'
+sudo cerbot -d ${subdomain}
 sudo cat >>  ${subdomain}/public/index.html <<EOL
         <h1>${subdomain} Test working</h1>
 EOL
