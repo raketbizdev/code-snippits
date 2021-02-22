3761f95ee7d193bb1b53734628dc456d41fca6a6
Ruel Nopal <radon1284@yahoo.com>
2021-02-22 11:46:39 +0800

    Update auto-install-apache2-aws.sh

--- auto-install-apache2-aws.sh
+++ auto-install-apache2-aws.sh
@@ -3,11 +3,12 @@
 # Company: Ruel Nopal - IT Consultant.
 # url: www.ruelnopal.com
 # Installing Apache in AWS inctance.
+# wget https://raw.githubusercontent.com/raketbizdev/code-snippits/master/auto-install-apache2-aws.sh; sudo chmod 755 auto-install-apache2-aws.sh; ./auto-install-apache2-aws.sh;
 
 echo "Make sure you are a sudo user"
 cd /
 sudo apt update
-sudo apt install apache2
+sudo apt install apache2 -y
 sudo ufw app list
 echo "Check if theres no error"
 sudo systemctl status apache2
