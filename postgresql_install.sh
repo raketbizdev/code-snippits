255449cef994bae7f28465a20a7b4776fd049d9b
Ruel Nopal <radon1284@yahoo.com>
2021-07-04 22:21:55 +0800

    Update postgresql_install.sh

--- postgresql_install.sh
+++ postgresql_install.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+# Author name: Ruel Nopal
+# Company: Ruel Nopal - IT Consultant.
+# url: www.ruelnopal.com
+# Installing NginX.
+# apt update && apt upgrade
+
+# apt install wget
+# run wget https://raw.githubusercontent.com/raketbizdev/code-snippits/master/postgresql_install.sh; sudo chmod 755 postgresql_install.sh; ./postgresql_install.sh
 
 # Create the file repository configuration:
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
@@ -11,3 +20,5 @@ sudo apt-get update
 # Install the latest version of PostgreSQL.
 # If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
 sudo apt-get -y install postgresql
+
+sudo rm postgresql_install.sh
