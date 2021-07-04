ac02c4e331e6efb6fa21ce8ad41297ef96bed368
Ruel Nopal <radon1284@yahoo.com>
2021-07-04 22:07:49 +0800

    Update setup-ruby-ubunto-20.04.sh

--- setup-ruby-ubunto-20.04.sh
+++ setup-ruby-ubunto-20.04.sh
@@ -28,7 +28,7 @@ ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//'
 
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
-source /usr/local/rvm/scripts/rvm
+source ~/.rvm/scripts/rvm
 rvm get stable --autolibs=enable
 u="$USER"
 usermod -a -G rvm $u
