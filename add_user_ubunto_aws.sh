cc4649ee79da6c23303330c9bb482997f83fe71d
Ruel Nopal <radon1284@yahoo.com>
2021-07-10 07:30:04 +0800

    Update add_user_ubunto_aws.sh

--- add_user_ubunto_aws.sh
+++ add_user_ubunto_aws.sh
@@ -23,9 +23,13 @@ sudo systemctl restart ssh
 
 sudo groups ${username}
 
+sudo usermod -aG sudo ${username}
+sudo adduser ${username} www-data
+sudo echo "${username}  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
 sudo mkdir /home/${username}/.ssh
 sudo touch /home/${username}/.ssh/authorized_keys
-sudo chown -R ${username:${username /home/${username}/.ssh
+sudo chown -R ${username}:${username} /home/${username}/.ssh
 echo 'Add public key to the authorized_keys:'
 echo 'Enter public_key from your local machine:'
 read pubkey
