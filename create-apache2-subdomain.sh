34357f0c374c9262256ca60600da679dd95e9bb2
Ruel Nopal <radon1284@yahoo.com>
2023-02-03 21:27:31 +0800

    Update create-apache2-subdomain.sh

--- create-apache2-subdomain.sh
+++ create-apache2-subdomain.sh
@@ -5,40 +5,49 @@
 # run the command below
 # wget https://raw.githubusercontent.com/raketbizdev/code-snippits/master/create-apache2-subdomain.sh; sudo chmod 755 create-apache2-subdomain.sh; ./create-apache2-subdomain.sh;
 
-echo 'create a subdomain folder'
-echo 'enter subdomain name:'
-read subdomain
+subdomain=""
 root_dir="${1:-${PWD}}"
-echo $root_dir
-sudo mkdir ${subdomain}
-echo "${subdomain} has been created."
-echo 'change user'
 
-sudo chown $USER:$USER ${subdomain}
-sudo mkdir ${subdomain}/public
-sudo touch ${subdomain}/public/index.html
-sudo chown $USER:$USER ${subdomain}/public/index.html
-echo 'creating ssl from certbot'
-sudo certbot certonly -d ${subdomain}
-sudo cat >>  ${subdomain}/public/index.html <<EOL
-        <h1>${subdomain} Test working</h1>
-EOL
-echo 'create a virtualhost'
-sudo touch ${subdomain}/${subdomain}.conf
-sudo chown $USER:$USER ${subdomain}/${subdomain}.conf
-sudo cat >> ${subdomain}/${subdomain}.conf <<EOL
+create_subdomain_folder() {
+  echo 'Creating subdomain folder...'
+  echo 'Enter subdomain name:'
+  read subdomain
+
+  sudo mkdir "$subdomain"
+  sudo chown "$USER:$USER" "$subdomain"
+
+  echo "${subdomain} has been created."
+  echo 'Change user...'
+}
+
+create_public_folder() {
+  sudo mkdir "${subdomain}/public"
+  sudo touch "${subdomain}/public/index.html"
+  sudo chown "$USER:$USER" "${subdomain}/public/index.html"
+}
+
+create_ssl_certificate() {
+  echo 'Creating SSL certificate...'
+  sudo certbot certonly -d "$subdomain"
+}
+
+create_virtualhost_file() {
+  echo 'Creating virtual host file...'
+  sudo touch "${subdomain}/${subdomain}.conf"
+  sudo chown "$USER:$USER" "${subdomain}/${subdomain}.conf"
+
+  cat >> "${subdomain}/${subdomain}.conf" <<EOL
 <VirtualHost *:80>
   ServerAdmin webmaster@localhost
-  ServerName ${subdomain}       
+  ServerName ${subdomain}
 </VirtualHost>
-
 <VirtualHost *:443>
-  ServerName ${subdomain} 
-  DocumentRoot $root_dir/${subdomain}/public
+  ServerName ${subdomain}
+  DocumentRoot ${root_dir}/${subdomain}/public
   ErrorLog \${APACHE_LOG_DIR}/${subdomain}.error.log
   CustomLog \${APACHE_LOG_DIR}/${subdomain}.access.log combined
   DirectoryIndex index.html index.cgi index.php
-  <Directory $root_dir/${subdomain}/public/>
+  <Directory ${root_dir}/${subdomain}/public/>
       Options Indexes FollowSymLinks
       AllowOverride All
       Require all granted
@@ -51,11 +60,23 @@ sudo cat >> ${subdomain}/${subdomain}.conf <<EOL
   SSLCertificateKeyFile /etc/letsencrypt/live/${subdomain}/privkey.pem
 </VirtualHost>
 EOL
-sudo ln -nfs "${root_dir}/${subdomain}/${subdomain}.conf" "/etc/apache2/sites-enabled/${subdomain}.conf"
-sudo cat ${root_dir}/${subdomain}/${subdomain}.conf
-sudo systemctl restart apache2
-echo 'end of the commanline'
-echo 'deleting shell script'
-sudo rm create-apache2-subdomain.sh
 
+  sudo ln -nfs "${root_dir}/${subdomain}/${subdomain}.conf" "/etc/apache2/sites-enabled/${subdomain}.conf"
+  sudo cat "${root_dir}/${subdomain}/${subdomain}.conf"
+}
 
+main() {
+  create_subdomain_folder
+  create_public_folder
+  create_ssl_certificate
+  create_virtualhost_file
+  
+  echo 'Restarting Apache service...'
+  sudo systemctl restart apache2
+
+  echo 'End of the command line'
+  echo 'Deleting shell script...'
+  sudo rm "$0"
+}
+
+main
