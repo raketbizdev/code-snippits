90ea0b8042e5e741426194eb2aceff4effcb1563
Ruel Nopal <radon1284@yahoo.com>
2023-02-10 19:14:03 +0800

    Update domain_config.sh
    update

--- domain_config.sh
+++ domain_config.sh
@@ -29,10 +29,6 @@ done
 # Install certbot if not already installed
 if ! [ -x "$(command -v certbot)" ]; then
   sudo apt-get update
-  sudo apt-get install software-properties-common
-  sudo add-apt-repository universe
-  sudo add-apt-repository ppa:certbot/certbot
-  sudo apt-get update
   sudo apt-get install certbot python3-certbot-apache
 fi
 
