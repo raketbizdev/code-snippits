cb891a3b3341c2b8454b504c07ab35201b3bda50
Ruel Nopal <radon1284@yahoo.com>
2023-02-13 17:11:35 +0800

    Update apache_domain_port80.sh

--- apache_domain_port80.sh
+++ apache_domain_port80.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
+#author: ruel nopal
+# url: rnopal.com
+# run this command below
+# bash <(curl -s https://raw.githubusercontent.com/raketbizdev/code-snippits/master/ss_domain_config.sh)
 
-# Define the domain name
-domain="example.com"
+# Function to validate FQDN
+function is_valid_fqdn() {
+    local domain=$1
+    local re="^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
+    if [[ $domain =~ $re ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Prompt for domain name
+while true; do
+    read -p "Enter the domain name (e.g. example.com): " domain
+    if is_valid_fqdn "$domain"; then
+        break
+    else
+        echo "Invalid domain name. Please enter a fully qualified domain name (FQDN)."
+    fi
+done
 
 # Create the document root directory
 sudo mkdir -p /var/www/$domain
