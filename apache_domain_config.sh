b0c6cc63059c54801df7e8fd25ffaa38027c6b1f
Ruel Nopal <radon1284@yahoo.com>
2023-02-13 19:34:15 +0800

    Update apache_domain_config.sh

--- apache_domain_config.sh
+++ apache_domain_config.sh
@@ -3,7 +3,7 @@
 #author: ruel nopal
 # url: rnopal.com
 # run this command below
-# bash <(curl -s https://raw.githubusercontent.com/raketbizdev/code-snippits/master/ss_domain_config.sh)
+# bash <(curl -s https://raw.githubusercontent.com/raketbizdev/code-snippits/master/apache_domain_config.sh)
 
 # Function to validate domain name
 function validate_domain {
