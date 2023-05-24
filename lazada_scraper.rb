a764c3e65615192c2dc8167a2fb90ff2ad41dcaf
Ruel Nopal <radon1284@yahoo.com>
2023-05-24 11:53:38 +0800

    Update lazada_scraper.rb

--- lazada_scraper.rb
+++ lazada_scraper.rb
@@ -1,50 +1,82 @@
-require 'open-uri'
+require 'uri'
 require 'nokogiri'
 require 'csv'
+require 'net/http'
 
+class LazadaScraper
+  attr_reader :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  # Scrape data from Lazada website
+  def scrape_data(max_page)
+    name = []
+    price = []
+    rating = []
+    sale = []
+
+    max_page.times do |i|
+      current_page_url = "#{url}?page=#{i + 1}"
+      page = fetch_page(current_page_url)
+      next unless page
+
+      # Extract data from page and append to respective arrays
+      name.concat(page.css('span.product-card__name').map(&:text).map(&:strip))
+      price.concat(page.css('div.product-card__price').map(&:text).map(&:strip))
+      rating.concat(page.css('span.rating__number').map(&:text).map(&:strip))
+      sale.concat(page.css('div.product-card__sale').map(&:text).map(&:strip))
+    end
+
+    [name, price, rating, sale]
+  end
+
+  private
+
+  # Fetch page content from the given URL
+  def fetch_page(url)
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    return unless response.is_a?(Net::HTTPSuccess)
+
+    Nokogiri::HTML(response.body)
+  end
+end
+
+class CsvWriter
+  attr_reader :filename
+
+  def initialize(filename)
+    @filename = filename
+  end
+
+  # Write data to a CSV file
+  def write_data(headers, data)
+    CSV.open(filename, 'w') do |file|
+      file << headers
+      data.each { |row| file << row }
+    end
+  end
+end
+
+# Set the URL to scrape
 url = 'http://www.lazada.com.ph/shop-computers-laptops/'
-page = Nokogiri::HTML(open(url))
 
-# Store data ins arrays
+# Set the maximum number of pages to scrape
+max_page = 5
 
-page_num = []
- 
-page.css('span.pages a[href]').each do |line|
-	page_num << line.text.strip
-end
-	# max_page = page_num.max
-	max_page = 5
-	name = []
-	price = []
-	rating = []
-	sale = []
+# Set the filename for the CSV output
+filename = 'lazada_scraper.csv'
 
-max_page.to_i.times do |i|
+# Instantiate the LazadaScraper
+lazada_scraper = LazadaScraper.new(url)
 
-	url = "http://www.lazada.com.ph/shop-computers-laptops/?page=#{i+1}"
-	page = Nokogiri::HTML(open(url))
- 
-	page.css('span.product-card__name').each do |line|
-		name << line.text.strip
-	end
+# Scrape data from Lazada website
+name, price, rating, sale = lazada_scraper.scrape_data(max_page)
 
-	page.css('div.product-card__price').each do |line|
-		price << line.text.strip
-	end
-	
-	page.css('span.rating__number').each do |line|
-		rating << line.text.strip
-	end
-	
-	page.css('div.product-card__sale').each do |line|
-		sale << line.text.strip
-	end
-end
+# Instantiate the CsvWriter
+csv_writer = CsvWriter.new(filename)
 
-# # Write data to csv files
-CSV.open('lazada_scraper.csv', 'w') do |file|
-	file << ["Product Name", "Price", "Rating", "Sale"]
-	name.length.times do |i|
-		file << [name[i], price[i], rating[i], sale[i]]
-	end
-end
+# Write data to the CSV file
+csv_writer.write_data(['Product Name', 'Price', 'Rating', 'Sale'], name.zip(price, rating, sale))
